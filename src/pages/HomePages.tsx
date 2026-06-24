dce2a34d2cd7bd68eb88329f950fe6426ef9c2a9
Wikan-dev <oneshootindo@gmail.com>
2026-06-24 10:42:40 +0800

    feature: user can test login from the localhost (before only from api client

--- src/pages/HomePages.tsx
+++ src/pages/HomePages.tsx
@@ -1,13 +1,15 @@
 import arrow from "../assets/svg/line-end-arrow.svg"
+import { Navigate, useNavigate } from "react-router-dom"
 
 export default function HomePages() {
+    const navigate = useNavigate();
     return (
         <div className="bg-primary-2 h-screen flex flex-col justify-center p-5">
             <div className="text-center mb-auto mt-auto">
                 <h1 className="text-black jost text-8xl">Rmndr.</h1>
                 <p className="text-black jost text-md">Your personal reminder app</p>
             </div>
-            <button className="w-full bg-primary-3 px-7.5 py-11 text-primary-1 flex items-center justify-between hover:bg-primary-4 transition-all duration-200 text-4xl font-jost rounded-cust mb-10">
+            <button onClick={() => navigate('/Login')} className="w-full bg-primary-3 px-7.5 py-11 text-primary-1 flex items-center justify-between hover:bg-primary-4 transition-all duration-200 text-4xl font-jost rounded-cust mb-10">
                 Get Started
                 <img src={arrow} alt="Arrow" className="w-15 h-15" />
             </button>
--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -2,22 +2,35 @@ import ButtonCustom from "../components/buttonCustom";
 import CustomInputTemplate from "../components/customInputTemplate";
 import googleLogo from "../assets/svg/google-icon-logo-svgrepo-com.svg"
 import { GoogleLogin } from "../function/googleLogin";
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import { useAuthStore } from "../store/authStore";
 
 export default function LoginPage() {
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
+    const { loginUser, isLoading: loading, error} = useAuthStore();
+
+    const handleSubmit = async (e: React.FormEvent) => {
+        e.preventDefault();
+
+        const isSucces = await loginUser(email, password);
+        if (isSucces) {
+            alert("login berhasil")
+        }
+    }
 
     return (
         <div className="p-5 bg-primary-2 h-screen">
+            {error ? <p className="text-red-500 text-center mb-4 font-jost text-lg font-semibold">{error}</p> :
+                <p className="text-primary-3 text-center mb-4 font-jost text-lg font-semibold">Login berhasil</p>
+            }
             <h1 className="font-jost text-8xl text-center mb-10">Rmndr.</h1>
             <form className="flex flex-col gap-5 mb-10" onSubmit={handleSubmit}>
                 <CustomInputTemplate disabled={loading} value={email} onChange={setEmail} title="Email" type="email" placeholder="Email here" />
                 <CustomInputTemplate disabled={loading} value={password} onChange={setPassword} title="Password" type="text" placeholder="Password here" />
+                <ButtonCustom text={loading ? "Mohon tunggu" : "Login"} disabled={loading}  />
             </form>
             <div className="flex flex-col gap-2">
-                <ButtonCustom text="Login"  />
                 <h1 className="font-jost text-md text-center cursor-default">or</h1>
                 <button type="submit" disabled={loading} onClick={GoogleLogin} className="bg-white hover:bg-gray-100 transition-all duration-200  py-4 px-7.5 flex gap-5 rounded-cust w-full items-center cursor-pointer"> 
                     <img className="w-10.5 h-10.5" src={googleLogo} alt="login with google" />
