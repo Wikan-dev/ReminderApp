e74c738b5b258800ae62d0991716215d199942d3
Wikan-dev <oneshootindo@gmail.com>
2026-07-22 20:54:40 +0800

    feat: add basic structure to reminder input page

--- src/pages/reminderInput.tsx
+++ src/pages/reminderInput.tsx
@@ -1,16 +1,32 @@
+import { useState } from "react"
+
 export default function NewReminder() {
+    const [hour, setHour] = useState<string>("");
+    const [minute, setMinute] = useState<string>("");
     //todo: selesain halaman nambahin reminder baru
     //todo ambil fungsi dari function addnewReminder 
     return (
-        <div>
-            <div>
+        <div className="bg-primary-1 h-screen">
+            <div className="">
                 <form action="">
-                    <input type="text" />
-                    <input type="text" />
-                    <input type="text" />
-                    <input type="text" />
+                    <div>
+                        <h1>Reminder Name</h1>
+                        <input type="text" />
+                    </div>
+                    <div>
+                        
+                    </div>
+                    <div>
+                        <h1>Reminder Name</h1>
+                        <input type="text" />
+                    </div>
+                    <div>
+                        <h1>Reminder Name</h1>
+                        <input type="text" />
+                    </div>
+                    
                 </form>
             </div>
         </div>
     )
-}
+} 
