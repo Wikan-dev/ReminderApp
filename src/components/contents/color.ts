01e661e4840c331e56712d050b38af08c887c30d
Wikan-dev <oneshootindo@gmail.com>
2026-07-25 08:50:45 +0800

    feat: added a page to add new reminder

---
+++ src/components/contents/color.ts
@@ -0,0 +1,9 @@
+export const COLLOR_PALLETE = [
+    { nama: "merah", hex: "#FF8989" },
+    { nama: "orange", hex: "#FFAC58" },
+    { nama: "kuning", hex: "#F2FF43" },
+    { nama: "lime", hex: "#88F855" },
+    { nama: "blue", hex: "#33C9FF" },
+    { nama: "soft purp", hex: "#CD83FF" },
+    { nama: "lavendar", hex: "#9900FF" },
+];
